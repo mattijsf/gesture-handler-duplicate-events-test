720ee1b487f92f224e466bb6ca4ac21438c25e9e
Mattijs Fuijkschot <mattijs@gaiyo.com>
2023-10-24 08:58:35 +0200

    updated sample with count label

--- App.tsx
+++ App.tsx
@@ -1,23 +1,20 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import {View} from 'react-native';
+import React, { useState } from 'react';
+import {Text, View} from 'react-native';
 import {
   GestureHandlerRootView,
   TouchableOpacity,
 } from 'react-native-gesture-handler';
 
 function App(): JSX.Element {
+  const [pressCount, setPressCount] = useState(0);
+
   return (
     <GestureHandlerRootView style={{flex: 1}}>
-      <View style={{flex: 1, backgroundColor: '#CCC', justifyContent: 'center', alignItems: 'center'}}>
-        <TouchableOpacity
-          onPress={(): void => {
-            console.log('onPress', Date.now());
-          }}
-          onPressIn={(): void => {
-            console.log('onPressIn', Date.now());
-          }}>
+      <View style={{flex: 1, backgroundColor: '#000', justifyContent: 'center', alignItems: 'center', gap: 10}}>
+      <Text style={{ color: '#fff' }}>Press Count: {pressCount}</Text>
+        <TouchableOpacity onPress={(): void => setPressCount(c => c + 1)}>
           <View style={{backgroundColor: '#ff0000', width: 100, height: 100}} />
         </TouchableOpacity>
       </View>
